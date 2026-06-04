0b4b2fd2920a76596913753b9383a657d9f2d6ad
cherish-vuppala <146354632+cherish-vuppala@users.noreply.github.com>
2026-06-04 07:32:04 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int rob(int[] nums) {
+      // at each element I have two choices
+      //  pick and skip
+      // choice 1 is to pick and choice 2 is to skip
+      // to avoid picking adjacent elements i'll hop with i + 2 if I pick
+      // and i + 1 if I skip
+      // return buid it bottom up and return the answer
+      int[] memo = new int[nums.length];
+      Arrays.fill(memo, -1);
+      return solve(0, nums, memo);
+
+    }
+
+    public int solve(int i, int[] nums, int[] memo) {
+        if (i >= nums.length) return 0; // nothing to rob
+
+        // if already computed return
+        if (memo[i] != -1) return memo[i];
+
+        int pick = nums[i] + solve(i+2, nums, memo);
+        int skip = solve(i+1, nums, memo);
+         
+        return memo[i] = Math.max(skip, pick);
+    }
+}
