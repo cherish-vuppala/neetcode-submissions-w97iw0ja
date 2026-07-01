918b82c8c9bb7273e62113c9d0b8492592cdb18d
cherish-vuppala <146354632+cherish-vuppala@users.noreply.github.com>
2026-07-01 07:12:26 +0530

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Boolean[] memo = new Boolean[s.length()];
+        Set<String> dictSet = new HashSet<>(wordDict);
+        return solve(0, s, dictSet, memo);
+    }
+
+    public boolean solve(int i, String s, Set<String> dictSet, Boolean[] memo) {
+        if (i >= s.length()) return true;
+        if (memo[i] != null) return memo[i];
+
+        for (int j = i; j < s.length(); j++) {
+            if (dictSet.contains(s.substring(i, j + 1))) {
+                //System.out.println(dictSet.contains(word));
+                if (solve(j + 1, s, dictSet, memo)) {
+                    return memo[i] = true;
+                }
+            }
+        }
+
+        return memo[i] = false;
+    }
+}
