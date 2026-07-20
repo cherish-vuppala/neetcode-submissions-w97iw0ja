e4ef23e105f7a00b205bd02810801ba8560b2003
cherish-vuppala <146354632+cherish-vuppala@users.noreply.github.com>
2026-07-20 19:32:28 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        var binary = Integer.toBinary(n);
+        var padding = ("0").repeat(32 - binary.length());
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(padding);
+        sb.append(binary);
+        sb.reverse();
+
+        return Integer.parseInt(sb.toString(), 2);
+    }
+}
