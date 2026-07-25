43bff0e579bb00c7f8d7d57b7449ffe718d2b197
cherish-vuppala <146354632+cherish-vuppala@users.noreply.github.com>
2026-07-25 15:54:38 +0530

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        return Math.max(
+            solve(nums, 0, n-2),
+            solve(nums, 1, n-1));
+    }
+
+    public int solve(int[] nums, int start, int end) {
+        if (start > end) return 0;
+
+        int[] dp = new int[end + 2];
+        
+
+        for (int i = start; i <= end + 1; i++) {
+            int pick = dp[i - 2] + nums[i-1];
+            int skip = dp[i - 1];
+            dp[i] = Math.max(pick, skip);
+        }
+
+        return dp[end];
+    }
+}
